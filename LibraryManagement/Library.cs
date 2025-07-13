f2c4b0a29cfbfaa2d7f5d8cccd8b7bbff8008aa3
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2025-07-13 18:16:07 +0100

    Created the Library class with a book list

---
+++ LibraryManagement/Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    public class Library
+    {
+        public List<Book> books = new List<Book>();
+
+        //methods to be created
+        //public void AddBook(Book book) { ... }
+        //public void ViewBooks() { ... }
+        //public Book SearchBookByTitle(string title) { ... }
+        //public void BorrowBook(int id) { ... }
+        //public void ReturnBook(int id) { ... }
+
+    }
+}
