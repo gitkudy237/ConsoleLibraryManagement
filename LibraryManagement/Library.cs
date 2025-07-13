736caf97d65b6e72d7cd19e6531220121df0d0e3
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2025-07-13 19:30:17 +0100

    Implemented the AddBook() method in the Library class.

--- LibraryManagement/Library.cs
+++ LibraryManagement/Library.cs
@@ -16,12 +16,31 @@ namespace LibraryManagement
         //public void SearchBookByTitle(string title) { ... }
         //public void SearchBookById(string id) { .. }
 
+
+
+        public void AddBook()
+        {
+            Console.WriteLine("Enter book Title: ");
+            string title = Console.ReadLine();
+            Console.WriteLine("Enter book author: ");
+            string author = Console.ReadLine();
+            Console.WriteLine("Enter the book ISBN: ");
+            string isbn = Console.ReadLine();
+            Console.WriteLine("Ente the number of pages: ");
+            int numberOfPages = Convert.ToInt32(Console.ReadLine());
+            var book = new Book(title, author, isbn, numberOfPages);
+            book.Id += 1;
+            books.Add(book);
+        }
+
+
         /* To be implemented by Kudy le dev */
-        public void RemoveBook(int id) { .. }
-        public void BorrowBook(int id)
-        { ... }
-        public void ReturnBook(int id) { ... }
-        public void DisplayMenu() { .. }
+        
+        //public void RemoveBook(int id) { .. }
+        //public void BorrowBook(int id)
+        //{ ... }
+        //public void ReturnBook(int id) { ... }
+        //public void DisplayMenu() { .. }
 
     }
 }
