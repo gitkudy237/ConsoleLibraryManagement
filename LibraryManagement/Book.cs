bad97a7e2c665fea924959a5650d5265553ed98d
kudy <bayardkudy1@gmail.com>
2025-07-13 17:35:36 +0100

    Created Book class

---
+++ LibraryManagement/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; } = string.Empty;
+    public string Author { get; } = string.Empty;
+    public string Isbn { get; set; } = string.Empty;
+    public int NumberOfPages { get; }
+    public bool IsAvailable { get; set; }
+
+    public Book(string title, string author, string isbn, int numberOfPages)
+    {
+        Title = title;
+        Author = author;
+        Isbn = isbn;
+        NumberOfPages = numberOfPages;
+        IsAvailable = true;
+    }
+}
