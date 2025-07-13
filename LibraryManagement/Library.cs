651594835c3571f744395df23e25f2421e2e8f64
kudy <bayardkudy1@gmail.com>
2025-07-13 19:16:16 +0100

    Implemented RemoveBook

--- LibraryManagement/Library.cs
+++ LibraryManagement/Library.cs
@@ -8,7 +8,12 @@ namespace LibraryManagement
 {
     public class Library
     {
-        public List<Book> books = new List<Book>();
+        public List<Book> books = new()
+        {
+            new Book("title1", "author1", "1239-27892-297", 150),
+            new Book("title2", "author2", "1452-28992-297", 3000),
+            new Book("title3", "author3", "1239-27892-297", 456),
+        };
 
         /* To be implemented by Clovis le pro */
         //public void AddBook(Book book) { ... }
@@ -16,12 +21,22 @@ namespace LibraryManagement
         //public void SearchBookByTitle(string title) { ... }
         //public void SearchBookById(string id) { .. }
 
-        /* To be implemented by Kudy le dev */
-        public void RemoveBook(int id) { .. }
+
+        public void RemoveBook(int id)
+        {
+            var existingBook = books.FirstOrDefault(b => b.Id == id);
+            if (existingBook is null)
+            {
+                Console.WriteLine("Book not found");
+                return;
+            }
+
+            books.Remove(existingBook);
+        }
         public void BorrowBook(int id)
-        { ... }
-        public void ReturnBook(int id) { ... }
-        public void DisplayMenu() { .. }
+        {  }
+        public void ReturnBook(int id) {  }
+        public void DisplayMenu() {  }
 
     }
 }
